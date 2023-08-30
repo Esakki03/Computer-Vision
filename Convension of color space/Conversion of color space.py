b7fede4e0aacc6517e9e1b1cec1a3344d90f0ca5
S ESAKKI RAJAN <113374677+Esakki03@users.noreply.github.com>
2023-08-30 09:59:15 +0530

    Add files via upload

---
+++ Convension of color space/Conversion of color space.py
@@ -0,0 +1,8 @@
+import cv2
+image = cv2.imread("C://Users//STAR//Pictures//OIP0.jpg")
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
+cv2.imshow('rgb',rgb_image)
+cv2.imshow('gray',gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
